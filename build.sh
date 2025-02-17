294e84382f806bb63c91dbcd4f03b952cb591579
nurfinzmuhamad14 <mnurdiansyah105@gmail.com>
2025-02-18 04:05:33 +0800

    generate configs

--- build.sh
+++ build.sh
@@ -13,18 +13,27 @@ export KBUILD_BUILD_USER=UdyneOS
 export KBUILD_BUILD_HOST=Droidx
 DEFCONFIG="RMX2195_defconfig"
 clear
-echo -e " "
+echo -e "  _____________________________
+         / I am Ready for Build Kernel. \\
+       \\     @UdyneOS                   /
+           -----------------------------
+         \   ^__^ 
+          \  (oo)\_______
+             (__)\       )\/\\
+                 ||----w |
+                 ||     ||
+     "
 echo -e "${txtbld}Config:${txtrst} $DEFCONFIG"
 echo -e "${txtbld}ARCH:${txtrst} arm64"
 echo -e "${txtbld}Username:${txtrst} $KBUILD_BUILD_USER"
-echo -e "$(make kernelversion)-release"
+echo -e "${txtbld}$(make kernelversion)-release"
 
 if [[ $1 == "-mr" || $1 == "--mrproper" ]]; then
 if [  -d "./out/" ]; then
 echo -e " "
         rm -rf  ./out/
 fi
-echo -e "Cleared"
+echo -e "Clean success"
 sleep 2
 fi
 echo -e "$blue    \nMake DefConfig\n $nocol"
@@ -34,4 +43,4 @@ make O=out ARCH=arm64 $DEFCONFIG
 sleep 2
 # Build start
 echo -e "$blue    \nStarting kernel compilation...\n $nocol"
-make -j$(nproc --all) O=out ARCH=arm64 CC="ccache clang" LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- CLANG_TRIPLE=aarch64-linux-gnu- Image.gz
+make -j$(nproc --all) O=out ARCH=arm64 CC="ccache clang" LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- CLANG_TRIPLE=aarch64-linux-gnu- 
